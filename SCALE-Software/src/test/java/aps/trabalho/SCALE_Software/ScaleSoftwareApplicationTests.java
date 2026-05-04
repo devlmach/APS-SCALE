fa63606bfd39aadf18f61c870b7abd5cc29af902
devlmach <machiavelli.rafa@gmail.com>
2026-05-03 22:25:08 -0300

    arquivos

---
+++ SCALE-Software/src/test/java/aps/trabalho/SCALE_Software/ScaleSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package aps.trabalho.SCALE_Software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaleSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/aps/trabalho/SCALE_Software/ScaleSoftwareApplication.java
@@ -0,0 +1,13 @@
+package aps.trabalho.SCALE_Software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaleSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaleSoftwareApplication.class, args);
+	}
+
+}
