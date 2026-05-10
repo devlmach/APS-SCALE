f975d336aff7b108d561430d1deb17e02897abb0
devlmach <machiavelli.rafa@gmail.com>
2026-05-10 16:24:12 -0300

    Ajuste da classe Main para inicar a interface gráfica

--- src/main/java/aps/trabalho/SCALE_Software/ScaleSoftwareApplication.java
+++ src/main/java/aps/trabalho/SCALE_Software/ScaleSoftwareApplication.java
@@ -1,12 +1,17 @@
 package aps.trabalho.SCALE_Software;
 
+import aps.trabalho.SCALE_Software.view.InterfaceFrame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class ScaleSoftwareApplication {
 
 	public static void main(String[] args) {
+        SwingUtilities.invokeLater(() ->
+                new InterfaceFrame().setVisible(true));
 		SpringApplication.run(ScaleSoftwareApplication.class, args);
 	}
 
