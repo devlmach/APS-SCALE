93f04841ed5919ad0db29bf7aaa1857958ff7909
devlmach <machiavelli.rafa@gmail.com>
2026-05-10 16:22:36 -0300

    Criação interface gráfica para inserção e busca dos dados

--- src/main/java/aps/trabalho/SCALE_Software/service/CalculadoraEmergia.java
+++ src/main/java/aps/trabalho/SCALE_Software/service/CalculadoraEmergia.java
@@ -1,4 +1,14 @@
 package aps.trabalho.SCALE_Software.service;
 
+import aps.trabalho.SCALE_Software.model.Recurso;
+
+import java.util.List;
+
 public class CalculadoraEmergia {
+
+    public static double calcularTotal(List<Recurso> recursos){
+        return recursos.stream()
+                .mapToDouble(Recurso::calcularEmergia)
+                .sum();
+    }
 }
